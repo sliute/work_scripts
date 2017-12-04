a320e1250d941027a67ca599245f882304f6a85f
Stefan Liute <stefanliute@gmail.com>
2017-12-04 16:07:02 +0000

    Update build_script.sh to work with just one project file with improved error handling.

--- build_release.sh
+++ build_release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Run this script from inside your Deployments folder, with two arguments, in this order:
+# - campus name, as used in Bitbucket (e.g. avado, ufw)
+# - release number (e.g. 17).
+#
+# $ ./build_release.sh avado 17
 
 # Create sub-folder structure if it doesn't already exist.
 # Pull project files repository from Bitbucket if needed.
@@ -12,54 +17,73 @@
 DIRS=(releases tmp)
 for dir in "$DIRS"
 do
-	if ! [ -d "$dir" ]; then
-		mkdir "$dir"
-	fi	
+    if ! [ -d "$dir" ]; then
+        mkdir "$dir"
+    fi
 done
 
 # Always have an up-to-date project repository.
 
 if ! [ -d alp-cm-projects ]; then
-	git clone git@bitbucket.org:avadolearning/alp-cm-projects.git
-	cd alp-cm-projects
+    git clone git@bitbucket.org:avadolearning/alp-cm-projects.git
+    if [ $? -eq 0 ]; then
+        cd alp-cm-projects
+    else
+        echo -e "\nFailure: could not clone the project repository.\nPlease make sure you can access it.\n"
+        exit
+    fi
 else
-	cd alp-cm-projects
-	git fetch
-	git reset --hard origin/master
+    cd alp-cm-projects
+    git fetch && git reset --hard origin/master
+    if [ $? -ne 0 ]; then
+        echo -e "\nFailure: could not update the project repository.\nPlease make sure you can access it."
+        exit
+    fi
 fi
 
-# Execute operations for each campus (name provided as parameter to this script).
+# Execute operations.
 
-DATE="$(date +%Y%m%d)"
+if ! [ -e ./sites/"$1".json ]; then
+    echo -e "\nFailure: the named project file does not exist.\nPlease check spelling and retry.\n"
+    exit
+fi
 
-for i in "$@"
-do
-	# Package a new release with Component Manager
-	componentmgr --verbose refresh --project-file ./sites/"$i".json
-	componentmgr --verbose package --project-file ./sites/"$i".json --package-format ZipArchive --package-destination ../tmp/"$DATE"-"$i".zip
+# Package a new release with Component Manager.
+componentmgr --verbose refresh --project-file ./sites/"$1".json && componentmgr --verbose package --project-file ./sites/"$1".json --package-format ZipArchive --package-destination ../tmp/"$(date +%Y%m%d)"-"$1".zip
 
-	# Clone the campus from Bitbucket, if it does not exist in the local releases folder.
-	if ! [ -d ../releases/"$i" ]; then
-		cd ../releases
-		git clone git@bitbucket.org:avadolearning/"$i".git
-		cd "$i"	
-	else
-		cd ../releases/"$i"
-	fi
+if [ $? -ne 0 ]; then
+    echo -e "\nFailure: Component Manager operations have failed.\nPlease check the content of your project file and your internet connectivity, then retry."
+    exit
+fi
 
-	# Put the new release in the campus and return to alp-cm-projects.
-	git fetch
-	git reset --hard origin/master
-	git clean -fdx
-	find . ! -path './.git*' -type f -exec git rm {} +
-	unzip -o ../../tmp/"$DATE"-"$i".zip
-	find . -name .gitignore -delete
-	git add .
-	cd ../../alp-cm-projects
-done
+# Clone the campus from Bitbucket, if it does not exist in the local releases folder.
+if ! [ -d ../releases/"$1" ]; then
+	cd ../releases
+	git clone git@bitbucket.org:avadolearning/"$1".git
+    if [ $? -eq 0 ]; then
+        cd "$1"
+    else
+        echo -e "\nFailure: could not clone the campus repository.\nPlease make sure you can access it.\n"
+        exit
+    fi
+else
+	cd ../releases/"$1"
+fi
 
+# Put the new release in the campus and return to alp-cm-projects folder.
+git fetch && git reset --hard origin/master
+if [ $? -ne 0 ]; then
+    echo -e "\nFailure: could not update the campus repository.\nPlease make sure you can access it."
+    exit
+fi
+git clean -fdx
+find . ! -path './.git*' -type f -exec git rm {} +
+unzip -o ../../tmp/"$(date +%Y%m%d)"-"$1".zip
+find . -name .gitignore -delete
+git add .
+git commit -m "ALP SP$(date +%Y).$2 release"
+cd ../../alp-cm-projects
 
 # Post completion message to console. 
-
-echo -e "\nDeployment script is complete.\nNow go into each release folder and commit with 'ALP SP2017.XX release'.\n"
+echo -e "\nSuccess: This deployment script has completed.\nNow go into the $1 campus folder, check the log and cherry-pick as needed.\n"
 
